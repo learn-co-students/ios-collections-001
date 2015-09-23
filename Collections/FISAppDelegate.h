af6f38350a53582b6d0b2ac8ac111d07a467b053
onowahoo <onowahoo@gmail.com>
2015-09-22 22:37:01 -0400

    this lab was worse

--- Collections/FISAppDelegate.h
+++ Collections/FISAppDelegate.h
@@ -6,10 +6,18 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-/**
- 
- * Declare your methods here.
- 
- */
+- (NSArray *)arrayBySortingArrayAscending:(NSArray *)arguement;
+- (NSArray *)arrayBySortingArrayDescending:(NSArray *)arguement;
+- (NSArray *)arrayBySwappingFirstObjectWithLastObjectInArray:(NSArray *)arguement;
+- (NSArray *)arrayByReversingArray:(NSArray *)arguement;
+- (NSString *)stringInBasicLeetFromString:(NSString *)arguement;
+- (NSArray *)splitArrayIntoNegativesAndPositives:(NSArray *)arguement;
+- (NSArray *)namesOfHobbitsInDictionary:(NSDictionary *)arguement;
+- (NSArray *)stringsBeginningWithAInArray:(NSArray *)arguement;
+- (NSNumber *)sumOfIntegersInArray:(NSArray *)arguement;   //HAD TO CHANGE THIS TO NSNUMBER IN ORDER FOR IT TO WORK
+- (NSArray *)arrayByPluralizingStringsInArray:(NSArray *)arguement;
+- (NSDictionary *)countsOfWordsInString:(NSString *)arguement;
+- (NSDictionary *)songsGroupedByArtistFromArray:(NSArray *)arguement;
+
 
 @end
